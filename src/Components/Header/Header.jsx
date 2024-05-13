a4f3ebf966c1acffd9ae11bbd771309c53da4eb9
mhshihab2604 <mhshihab2604@gmail.com>
2024-05-13 23:30:12 +0600

    Dark Theme Add Done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import useAuth from "../useAuth/useAuth";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import myImage from "../../assets/logo22.png"
 const Header = () => {
   const { logout, user } = useAuth();
@@ -10,6 +10,24 @@ const Header = () => {
     setShowDropdown(false)
     logout().then().catch();
   };
+
+  const [theme, setTheme] = useState('light')
+  const handleToggle = e => {
+    if (e.target.checked) {
+        setTheme('synthwave')
+    } else {
+        setTheme('light')
+    }
+}
+useEffect(() => {
+    localStorage.setItem('theme', theme)
+    const localTheme = localStorage.getItem('theme')
+
+    // add custom data-theme attribute
+    document
+        .querySelector('html')
+        .setAttribute('data-theme', localTheme)
+}, [theme])
   return (
     <div>
       <div className="navbar pt-0 pb-0">
@@ -122,6 +140,38 @@ const Header = () => {
             </Link>
           </div>
         )}
+        <label className="cursor-pointer grid place-items-center ml-2">
+          <input
+              onChange={handleToggle}
+              type="checkbox"
+              value="synthwave"
+              className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"/>
+          <svg
+              className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path
+              d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+          <svg
+              className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+              xmlns="http://www.w3.org/2000/svg"
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round">
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+      </label>
       </div>
     </div>
   );
