1e7ffbbc21238989b2778d5adcab50caf895b141
mhshihab2604 <mhshihab2604@gmail.com>
2024-05-14 16:31:13 +0600

    Changing the sorting option styles

--- src/Components/NeedsVolunteer/NeedsVolunteer.jsx
+++ src/Components/NeedsVolunteer/NeedsVolunteer.jsx
@@ -20,9 +20,9 @@ const NeedsVolunteer = () => {
                     <h1 className='lg:text-4xl text-2xl text-center font-medium'>Volunteer Needs Now</h1>
                     <p>The majority of volunteer management platforms have undergone alterations, <br /> often impacted by injected humor or randomized words that lack believability</p>
                 </div>
-                <div className="max-w-6xl mx-auto mt-10 ml-10">
-                    <h1 className="text-2xl">Filter :</h1>
-                    <select className="p-2 border-2 w-36 bg-[#38AA95] text-white border-indigo-300 rounded" value={sorting} onChange={(e) => setSorting(e.target.value)}>
+                <div className="max-w-6xl flex justify-center gap-2 items-center mx-auto mt-10">
+                    <h1 className="text-2xl font-bold">Filter :</h1>
+                    <select className="p-2 border-2 w-36 hover:bg-[#38AA95] text-black hover:text-white font-semibold border-indigo-300 rounded" value={sorting} onChange={(e) => setSorting(e.target.value)}>
                         <option value="0">All</option>
                         <option value="1">New First</option>
                         <option value="-1">OldFirst</option>
